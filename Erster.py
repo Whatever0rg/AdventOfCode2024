510a8f4caed6d66805e7c2d8e54b03cbeeb7ee12
Bennet Hoffjann <bennet.hoffjann@icloud.com>
2024-12-01 18:09:45 +0100

    touch ups

--- Erster.py
+++ Erster.py
@@ -17,27 +17,22 @@ for i, number in enumerate(numbers):
 
 SumListe = []
 SpaceListe = []
-print(Liste1,';',Liste2)
+
 
 def SumOfSpaces(Liste1,Liste2):
     Liste1.sort()
     Liste2.sort()
-    print(Liste1,';',Liste2)
-
 
     for i in range(len(Liste1)):
         a = Liste1[i]-Liste2[i]
         SpaceListe.append(a)
 
-
-    print(SpaceListe)
     for value in SpaceListe:
         if value < 0:
             SumListe.append(-value)
         else:
             SumListe.append(value)
 
-    print(SumListe)
     Sum = sum(SumListe)
 
     print(Sum)
@@ -47,13 +42,14 @@ def SumOfTimes(Liste1,Liste2):
 
     counter = Counter(Liste2)
     CountListe = [counter[element] for element in Liste1]
-    print(CountListe)
+    
 
     for i in range(len(Liste1)):
         SumListe.append(Liste1[i]*CountListe[i])
-    print(SumListe)
+
     Sum = sum(SumListe)
 
     print(Sum)
 
+SumOfSpaces(Liste1,Liste2)
 SumOfTimes(Liste1,Liste2)
